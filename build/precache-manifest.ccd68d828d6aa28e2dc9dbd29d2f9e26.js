cb2f86807e209fbd59ff3c26f94e4ce070f81e49
bettermetalzsnake <marcmartinezemail@gmail.com>
2019-11-10 21:33:44 -0700

    first commit

---
+++ build/precache-manifest.ccd68d828d6aa28e2dc9dbd29d2f9e26.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d708276b4cafba743ca8cbd9c7cd505",
+    "url": "/index.html"
+  },
+  {
+    "revision": "52773c3ed4a07bf56c13",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "05f398471a2b350d5acd",
+    "url": "/static/js/2.9ffd1bd3.chunk.js"
+  },
+  {
+    "revision": "52773c3ed4a07bf56c13",
+    "url": "/static/js/main.331d0b51.chunk.js"
+  },
+  {
+    "revision": "500c6a4d9d125d185445",
+    "url": "/static/js/runtime-main.4ea2b256.js"
+  }
+]);
