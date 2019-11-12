e0800cdf24630286d425316c5952859e8cd05bf3
bettermetalzsnake <marcmartinezemail@gmail.com>
2019-11-11 20:30:03 -0700

    updated h2

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ class App extends Component {
         return (
           <form onSubmit={this.loginUser}>
             <h1>An Awesome Form on React</h1>
-            <h2> updated from ci cd</h2>
+            <h2>updated from ci cd</h2>
             <label htmlFor="email">
               <span>Email:</span>
               <input onChange={this.inputChange} value={email} id="email" name="email" type="email" />
